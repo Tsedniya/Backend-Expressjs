ec1777fff495d110061b61b33c1c91e9c764f9bf
Tsedniya <ktsedniya@gmail.com>
2025-09-25 20:09:10 +0300

    backend:now

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express()
 const PORT = 3000
 
 app.get('/',(req,res) =>{
-    console.log('no it ',req.method)
+    console.log('',req.method)
     res.sendStatus(201)
 })
 
 app.get('/auth',(req,res) =>{
-    console.log('yes  auth',)
+    console.log('',)
     res.send('by')
 })
 app.listen(PORT,()=> console.log(`server started ${PORT}`))
