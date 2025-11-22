0aef7fdb38387310102554b115c9a37a0906e55f
Tsedniya <ktsedniya@gmail.com>
2025-11-22 22:16:49 +0300

    backend:get

--- server.js
+++ server.js
@@ -2,13 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-app.get('/',(req,res) =>{
-    console.log('nu',req.method)
-    res.sendStatus(201)
+app.get('/', (req, res)=>{
+    console.log('yay i hit an endpoint', req.method)
+    res.sendStatus(200)
 })
 
-app.get('/auth',(req,res) =>{
-    console.log('',)
-    res.send('b')
-})
 app.listen(PORT,()=> console.log(`server started ${PORT}`))
