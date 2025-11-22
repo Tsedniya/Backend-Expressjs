7207550840fc9797cd51d38a0298be83ea0a79bb
Tsedniya <ktsedniya@gmail.com>
2025-11-22 23:41:59 +0300

    backend:test

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+let data = {
+    name:"tutu"
+}
+
 app.get('/', (req, res)=>{
     res.send('<h1>Home</h1>')
 })
@@ -9,6 +13,11 @@ app.get('/', (req, res)=>{
 
 app.get('/dashboard', (req, res)=>{
     res.send('<h1>dashboard</h1>')
+})
+
+app.get('/api/data',(req,res)=>{
+    console.log('this one was for data')
+    res.send(data)
 })
 
 app.listen(PORT,()=> console.log(`server started ${PORT}`))
