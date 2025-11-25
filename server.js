a4f4b4a9ee83e719213881c57ee05f3550f08e45
Tsedniya <ktsedniya@gmail.com>
2025-11-25 21:40:06 +0300

    backend:testapi

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-let data = {
-    name:"tutu"
-}
+let data = ["james"]
+
+app.use(express.json())
 
 app.get('/', (req, res)=>{
     res.send('<h1>Home</h1>')
@@ -17,11 +17,17 @@ app.get('/dashboard', (req, res)=>{
 
 app.get('/api/data', (req, res) => {
     res.send(`
-        <body style="background:pink; color:blue;">
+        <body style="background:red; color:blue;">
             <h1>DATA:</h1>
             <p>${JSON.stringify(data)}</p>
         </body>
     `)
+})
+
+app.post('/api/data', (req,res)=>{
+    const newEntery = req.body
+    console.log(newEntery)
+    res.sendStatus(201)
 })
 
 
