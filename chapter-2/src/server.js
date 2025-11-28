548c956258f0267b3d946f569f3022d73ff78d7e
Tsedniya <ktsedniya@gmail.com>
2025-11-28 16:58:52 +0300

    Add chapter-2 files

---
+++ chapter-2/src/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// get file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// serve static files from src/public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`server has started on port: ${PORT}`);
+});
