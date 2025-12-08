5c9de3d694d03b03e54c20f70f295eabf15e7dfc
Tsedniya <ktsedniya@gmail.com>
2025-12-09 02:54:30 +0300

    backend:auth

--- chapter-2/src/routes/authRoutes.js
+++ chapter-2/src/routes/authRoutes.js
@@ -7,6 +7,20 @@ import db from '../db.js'
 const router = express.Router()
 
 router.post('/register', (req, res)=>{
+    const {username, password}= req.body
+    const hashedPassword = bcrypt.hashSync(password,8)
+    console.log(hashedPassword)
+    
+
+    try{
+        const insertUser = db.prepare(`INSERT INTO users (username,password) VALUES (?,?)`)
+        const result = insertUser.run(username,hashedPassword)
+        
+        const defaultTodo = `Hello! Add your first todo`
+    }catch(err){
+        console.log(err.message)
+        res.sendStatus(503)
+    }
 
 })
 
