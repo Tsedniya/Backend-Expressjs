b06560863f6674850992d0ff9cce85e892cb65b8
Tsedniya <ktsedniya@gmail.com>
2026-01-22 22:45:30 +0300

    chapter-3

---
+++ chapter-3/src/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js'
+import authMiddleware from './middleware/authMiddleware.js'
+
+
+const app = express();
+const PORT = process.env.PORT || 5003;
+
+// get file path from the url of the  current module
+const __filename = fileURLToPath(import.meta.url);
+//get the directory name from the file path 
+const __dirname = dirname(__filename);
+
+app.use(express.json())
+// serve static files from src/public
+app.use(express.static(path.join(__dirname, 'public')));
+ 
+// serve html file from the /pubic directory
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use('/auth',authRoutes)
+app.use('/todos', authMiddleware ,todoRoutes)
+
+app.listen(PORT, () => {
+  console.log(`server has started on port: ${PORT}`);
+});
